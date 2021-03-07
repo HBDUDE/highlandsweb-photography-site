bfcf19f8cb3942d086e49c8bfe092fb9e6fa3aa5
HBDUDE <supersuniiside@gmail.com>
2021-03-06 22:39:52 -0700

    Added fixed position info, adjusted section padding

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 import './App.css';
 import Navbar from './components/Navbar.jsx'
 import About from './components/About.jsx'
@@ -8,9 +10,12 @@ import Footer from './components/Footer.jsx'
 import camera2 from './images/johnny_automatic_digital_camera.png'
 
 function App() {
+  // const [scrollSection, setScrollSection] = useState("#top")
+
   return (
     <div className="App">
       <img src={camera2} alt="vintage camera clip art" className="image cam2"/>
+      {/* <div id="next-section-button"><a>Next Section</a></div> */}
       <Navbar/>
       <About/>
       <Gallery/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
       <nav className="nav-container">
-        <h1>
-          <a className="title text-primary" href="#top">Highland's Web Photography</a>
-        </h1>
-        
-        <div className="down-chevron">
-          
+        <div className="inner-nav">
+          <h1>
+            <a className="title text-primary" href="#top">Highland's Web Photography</a>
+          </h1>
         </div>
       </nav>
   )
